87cfeaf0bdfbe757ca5ea4d7b03536e4dfad0d61
Billy <55503825+bmortreux@users.noreply.github.com>
2021-12-15 20:40:43 +0100

    Affichage Annuaire companies :lipstick:

--- front/src/app/annuaire/annuaire.component.ts
+++ front/src/app/annuaire/annuaire.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {PersonnesService} from "../services/personnes.service";
 // @ts-ignore
 import {personnes} from '../models/personnes';
+import { companies } from '../models/companies';
+import {CompaniesService} from "../services/companies.service";
 
 @Component({
   selector: 'app-annuaire',
@@ -14,14 +16,24 @@ export class AnnuaireComponent implements OnInit {
 
   public persons: personnes[];
 
-  constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private personsService : PersonnesService, private router: Router) {
+  // @ts-ignore
+  public companies: companies[];
+
+  constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private personsService : PersonnesService,
+              private companiesService: CompaniesService, private router: Router) {
     this.persons= [];
+    this.companies= [];
+    //on a besoin de faire un this.ngOnInit() car après valider le form et revenir avec le .navigate il faut maj le component
+    this.ngOnInit();
   }
 
   ngOnInit(): void {
     this.personsService.getPersonnesList().subscribe((personResponse => {
       this.persons = personResponse;
-    }))
+    }));
+    this.companiesService.getCompaniesList().subscribe((companiesResponse => {
+      this.companies = companiesResponse;
+    }));
   }
 
   deletePerson(id: number): void {
